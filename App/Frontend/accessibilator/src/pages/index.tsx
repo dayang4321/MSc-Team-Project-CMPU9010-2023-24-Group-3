5a614d6eeaab92fc5828c6cebac4d3db26796960
Dave <davetherubix@gmail.com>
2023-10-16 09:48:12 +0100

    implement tw styling on homepage

--- App/Frontend/accessibilator/src/pages/index.tsx
+++ App/Frontend/accessibilator/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import React, { useState } from 'react';
 import FileUploadButton from '../components/FileUploadButton';
 import PopupMessage from '../components/PopupMessage';
@@ -14,116 +13,37 @@ export default function Home() {
     }, 3000);
   };
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Accessibilator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>Welcome to the Accessibilator!</h1>
+      <main className="flex min-h-screen flex-col text-center justify-center items-center flex-1 bg-slate-50 text-gray-900">
+        <h1 className="text-5xl max-w-4xl font-bold mb-11">
+          Reading documents made more accessible😁
+        </h1>
 
-        <p className={styles.description}>
-          Improving accessibility of documents for ease of access
-        </p>
+        <div className="bg-gray-900 p-12 pb-16 max-w-4xl text-white">
+          <h2 className="text-4xl max-w-4xl font-semibold mb-4">
+            Upload a document
+          </h2>
+          <p className="mb-5">
+            Supported documents include Word(docx), Powerpoint(ppt), Excel
+            (xlsx).
+          </p>
+          <div>
+            <p className="mb-3">Select a file or drag and drop here</p>
+          </div>
+          <FileUploadButton onUploadSuccess={handleUploadSuccess} />
+        </div>
 
-        <FileUploadButton onUploadSuccess={handleUploadSuccess} />
         {showMessage && (
           <PopupMessage type="success" content="File Uploaded successfully" />
         )}
-
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
 
-      <footer>
-        {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a> */}
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      <footer>Footer</footer>
+    </>
   );
 }
