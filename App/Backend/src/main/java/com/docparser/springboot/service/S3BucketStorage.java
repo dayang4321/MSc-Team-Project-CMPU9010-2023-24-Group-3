11a3372e94664c2999707a368466dfe11d016a1e
Cheril <cheriljohn1227@gmail.com>
2023-10-21 07:53:31 +0100

    Added REST API  endpoint to download file

--- App/Backend/src/main/java/com/docparser/springboot/service/S3BucketStorage.java
+++ App/Backend/src/main/java/com/docparser/springboot/service/S3BucketStorage.java
@@ -1,20 +1,18 @@
 package com.docparser.springboot.service;
 
+
 import com.docparser.springboot.model.S3StorageInfo;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.*;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Date;
 import java.util.Objects;
 
@@ -24,16 +22,12 @@ public class S3BucketStorage {
 
     @Value("${amazonProperties.bucketName}")
     private String bucketName;
-
     @Value("${amazonProperties.region}")
     private String region;
 
-    @Value("${amazonProperties.accessKey}")
-    private String accessKey;
-    @Value("${amazonProperties.secretKey}")
-    private String secretKey;
 
-
+    @Autowired
+    private S3Client s3Client;
     private File convertMultiPartToFile(MultipartFile file) throws IOException {
         File convFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
         FileOutputStream fos = new FileOutputStream(convFile);
@@ -41,29 +35,29 @@ public class S3BucketStorage {
         fos.close();
         return convFile;
     }
+
     private String generateFileName(MultipartFile multiPart) {
         return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
     }
-    private PutObjectResponse  uploadFileToS3(String key, File file) {
-        AwsCredentials credentials = AwsBasicCredentials.create(this.accessKey, this.secretKey);
-        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(credentials);
-        S3Client s3Client = S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(credentialsProvider)
-                .build();
+
+    private PutObjectResponse uploadFileToS3(String key, File file) throws IOException {
+
 
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
                 .build();
 
+
         PutObjectResponse response = s3Client.putObject(putObjectRequest, file.toPath());
         System.out.println("File uploaded successfully. ETag: " + response.eTag());
+
         return response;
     }
+
     private String getUploadedObjectUrl(String fileName) {
 
-        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region,fileName);
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
     }
 
     public S3StorageInfo uploadFile(MultipartFile multipartFile) {
@@ -73,11 +67,38 @@ public class S3BucketStorage {
             PutObjectResponse s3response = uploadFileToS3(fileName, file);
             String fileUrl = getUploadedObjectUrl(fileName);
             file.delete(); // Delete the temporary file after successful upload
-            return new S3StorageInfo(s3response.eTag(), fileUrl);
+            return new S3StorageInfo(s3response.eTag(), fileUrl,fileName);
         } catch (Exception e) {
             e.printStackTrace(); // Consider logging the exception instead of printing it
         }
         return null; // Return null if an error occurs
+    }
+
+    public InputStream getFileStreamFromS3(String key){
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+        ResponseBytes<GetObjectResponse> objectBytes = s3Client.getObjectAsBytes(getObjectRequest);
+        byte[] data = objectBytes.asByteArray();
+        InputStream inputStream = new ByteArrayInputStream(data);
+        return  inputStream;
+    }
+    public FileSystemResource download(String key) throws IOException {
+
+        InputStream inputStream=getFileStreamFromS3(key);
+        File tempFile = File.createTempFile("downloadedFile", ".docx");
+        tempFile.deleteOnExit();
+        try (XWPFDocument document = new XWPFDocument(inputStream)) {
+
+            FileOutputStream out = new FileOutputStream(tempFile);
+            document.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception
+        }
+        // Write the data to a local file.
+        return new FileSystemResource(tempFile);
     }
 
 }
