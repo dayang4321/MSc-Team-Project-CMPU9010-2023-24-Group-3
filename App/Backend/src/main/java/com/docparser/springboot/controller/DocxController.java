bbf464e5be14f631ee311a2c2a87d2c10501d98b
Cheril <cheriljohn1227@gmail.com>
2023-10-21 07:58:11 +0100

    added REST API endpoint to chnage font type of file

--- App/Backend/src/main/java/com/docparser/springboot/controller/DocxController.java
+++ App/Backend/src/main/java/com/docparser/springboot/controller/DocxController.java
@@ -1,31 +1,38 @@
 package com.docparser.springboot.controller;
 
-import com.docparser.springboot.model.S3StorageInfo;
-import com.docparser.springboot.service.S3BucketStorage;
+import com.docparser.springboot.service.DocumentParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-
-
+import java.io.IOException;
 
 @RestController
 public class DocxController {
     @Autowired
-    private S3BucketStorage s3FileUploadService;
+    private DocumentParser documentParser;
 
-    @PostMapping("/uploadFile")
-    public ResponseEntity<S3StorageInfo> fileUploading(@RequestParam("file") MultipartFile file)  {
+    @PostMapping("/parseDocToChangeFontType")
+    public ResponseEntity<Resource> changeFont(@RequestParam("filename") String fileName) throws IOException {
         // Code to save the file to a database or disk
-        S3StorageInfo storageInfo = s3FileUploadService.uploadFile(file);
 
-        return ResponseEntity.ok(storageInfo);
+        Resource resource = documentParser.changeFontType(fileName);
+
+        HttpHeaders headers = new HttpHeaders();
+        String modifiedFileName = documentParser.getUnigueFileName("FontTypeModifiedFile");
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + modifiedFileName);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+
     }
-
-
 
 }
---
+++ App/Backend/src/main/java/com/docparser/springboot/service/DocumentParser.java
@@ -0,0 +1,57 @@
+package com.docparser.springboot.service;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTFontSize;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTFonts;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTHpsMeasure;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTRPr;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Random;
+
+@Service
+public class DocumentParser {
+    @Autowired
+    private S3BucketStorage s3FileUploadService;
+
+    public String getUnigueFileName(String appliedChange) {
+        String uniqueFileName = appliedChange + System.currentTimeMillis() + "_" + new Random().nextInt(1000) + ".docx";
+        return uniqueFileName;
+    }
+
+    public FileSystemResource changeFontType(String key) throws IOException {
+        InputStream inputStream = s3FileUploadService.getFileStreamFromS3(key);
+        File tempFile = File.createTempFile("modifiedFile", ".docx");
+        tempFile.deleteOnExit();
+
+        try (XWPFDocument document = new XWPFDocument(inputStream)) {
+            for (XWPFParagraph paragraph : document.getParagraphs()) {
+                for (XWPFRun run : paragraph.getRuns()) {
+                    CTRPr rpr = run.getCTR().isSetRPr() ? run.getCTR().getRPr() : run.getCTR().addNewRPr();
+                    CTFonts fonts = rpr.isSetRFonts() ? rpr.getRFonts() : rpr.addNewRFonts();
+
+                    // Set the font family to "Open Sans"
+                    fonts.setAscii("Open Sans");
+                    fonts.setHAnsi("Open Sans");
+                    fonts.setCs("Open Sans");
+
+                }
+            }
+
+            FileOutputStream out = new FileOutputStream(tempFile);
+            document.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception
+        }
+
+        return new FileSystemResource(tempFile);
+    }
+
+}
