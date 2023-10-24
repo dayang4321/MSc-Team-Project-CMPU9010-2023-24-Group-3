ab01f99384fa27b8ba743ee0f110cefdcfbe8fec
Dave <davetherubix@gmail.com>
2023-10-24 10:24:02 +0100

    add text logo to default layout

--- App/Frontend/accessibilator/src/layouts/DefaultLayout.tsx
+++ App/Frontend/accessibilator/src/layouts/DefaultLayout.tsx
@@ -7,8 +7,8 @@ interface LayoutProps {
 const DefaultLayout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <nav className="p-5">
-        <div>Logo</div>
+      <nav className="px-8 py-5">
+        <div className="text-2xl uppercase font-extrabold">Accessibilator</div>
       </nav>
       {children}
       <footer className="p-5">Footer</footer>
