02171ac49c7191da8ebd0d85a0d61288e800a8d5
Cheril <cheriljohn1227@gmail.com>
2023-11-02 18:15:01 +0000

    renamed files part of getToken info

---
+++ App/Backend/src/main/java/com/docparser/springboot/model/SessionInfo.java
@@ -0,0 +1,77 @@
+package com.docparser.springboot.model;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbConvertedBy;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+import java.time.Instant;
+import java.util.List;
+
+@DynamoDbBean
+public class SessionInfo {
+
+    private String sessionID;
+    private String ipAddress;
+    private String tokenID;
+    private Instant createdDate;
+
+    private List<FeedBackForm> feedBackForms;
+
+
+    @DynamoDbAttribute("ipAddress")
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public SessionInfo(String sessionID, String ipAddress, String tokenID, Instant createdDate) {
+        this.ipAddress = ipAddress;
+        this.tokenID = tokenID;
+        this.createdDate = createdDate;
+        this.sessionID = sessionID;
+    }
+
+    public SessionInfo() {
+        // Initialization code if needed
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    @DynamoDbAttribute("token")
+    public String getTokenID() {
+        return tokenID;
+    }
+
+    public void setTokenID(String tokenID) {
+        this.tokenID = tokenID;
+    }
+
+    @DynamoDbAttribute("createdDate")
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("sessionID")
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+    @DynamoDbAttribute("feedbackForms")
+    public List<FeedBackForm> getFeedBackForms() {
+        return feedBackForms;
+    }
+
+    public void setFeedBackForms(List<FeedBackForm> feedBackForms) {
+        this.feedBackForms = feedBackForms;
+    }
+}
---
+++ App/Backend/src/main/java/com/docparser/springboot/service/SessionService.java
@@ -0,0 +1,66 @@
+package com.docparser.springboot.service;
+
+import com.docparser.springboot.Repository.SessionRepository;
+import com.docparser.springboot.model.FeedBackForm;
+import com.docparser.springboot.model.SessionInfo;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class SessionService {
+    @Autowired
+    SessionRepository sessionRepository;
+    private static final String SECRET_KEY = "ana7263nsnakka838";
+    public String generateToken(String ipAddress, String sessionID){
+    Instant now = Instant.now();
+    Instant expirationTime = now.plusSeconds(3600);
+        return Jwts.builder().setId(sessionID)
+                .setSubject(ipAddress)
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(expirationTime))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+
+}
+    public String getIpAddressFromToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+    }
+    public String getSessionIdFromToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getId();
+    }
+
+
+    public String generateAndSaveUserInfo(String ipAddress){
+        SessionInfo session= new SessionInfo();
+       String sessionID= UUID.randomUUID().toString();
+        String token = generateToken(ipAddress,sessionID);
+        session.setCreatedDate(Instant.now());
+        session.setIpAddress(ipAddress);
+        session.setTokenID(token);
+        session.setSessionID(sessionID);
+        sessionRepository.save(session);
+        return token;
+    }
+
+    public SessionInfo saveFeedbackInfo(String token, FeedBackForm feedBackForm){
+        SessionInfo session= new SessionInfo();
+        List<FeedBackForm> feedBackFormList =new ArrayList<FeedBackForm>();
+        feedBackFormList.add(feedBackForm);
+        String sessionID= getSessionIdFromToken(token);
+        session.setSessionID(sessionID);
+        session.setFeedBackForms(feedBackFormList);
+        sessionRepository.save(session);
+        SessionInfo sessionInfo = sessionRepository.getSessionInfo(sessionID);
+        return sessionInfo;
+    }
+}
+
+
