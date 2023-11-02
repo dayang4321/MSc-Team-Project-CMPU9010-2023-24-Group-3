e085e517e2d2151ea1ea1ddfa64b9972abd982e3
Cheril <cheriljohn1227@gmail.com>
2023-11-02 18:13:52 +0000

    modifiled token implementation

--- App/Backend/src/main/java/com/docparser/springboot/controller/DocxController.java
+++ App/Backend/src/main/java/com/docparser/springboot/controller/DocxController.java
@@ -1,16 +1,17 @@
 package com.docparser.springboot.controller;
 
+import com.docparser.springboot.model.FeedBackForm;
 import com.docparser.springboot.model.S3StorageInfo;
+import com.docparser.springboot.model.SessionInfo;
 import com.docparser.springboot.service.DocumentParser;
+import com.docparser.springboot.service.SessionService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -19,6 +20,8 @@ import java.io.IOException;
 public class DocxController {
     @Autowired
     private DocumentParser documentParser;
+    @Autowired
+    private SessionService sessionService;
 
     @GetMapping("/parseDocToChangeFontType")
     public ResponseEntity<Resource> changeFont(@RequestParam("filename") String fileName) throws IOException {
@@ -43,7 +46,7 @@ public class DocxController {
         // Code to save the file to a database or disk
         Resource resource = documentParser.increaseFont(fileName);
         HttpHeaders headers = new HttpHeaders();
-        String modifiedFileName= documentParser.getUniqueFileName("FontSizeModifiedFile");
+        String modifiedFileName = documentParser.getUniqueFileName("FontSizeModifiedFile");
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + modifiedFileName);
         return ResponseEntity.ok()
                 .headers(headers)
@@ -53,13 +56,23 @@ public class DocxController {
     }
 
 
-     @GetMapping("/modifyFile")
-    public ResponseEntity<S3StorageInfo> modifyDocument(@RequestParam("filename") String fileName) throws IOException  {
+    @GetMapping("/modifyFile")
+    public ResponseEntity<S3StorageInfo> modifyDocument(@RequestParam("filename") String fileName) throws IOException {
         // Code to save the file to a database or disk
         S3StorageInfo storageInfo = documentParser.modifyFile(fileName);
         return ResponseEntity.ok(storageInfo);
     }
 
-    
+    @GetMapping("/feedback")
+    public ResponseEntity<SessionInfo> feedback(@RequestBody FeedBackForm feedBackForm, HttpServletRequest request) throws IOException {
+        // Code to save the file to a database or disk
+        String token = request.getHeader("Authorization");
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+        SessionInfo sessionInfo = sessionService.saveFeedbackInfo(token, feedBackForm);
+        return ResponseEntity.ok(sessionInfo);
+    }
+
 
 }
