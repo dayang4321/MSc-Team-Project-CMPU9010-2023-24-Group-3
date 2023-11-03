a03c8c9b44e50ceb84af7f29d2dc7409b0629cd2
Dave <davetherubix@gmail.com>
2023-11-03 08:59:38 +0100

    increase dialog modal max width

--- App/Frontend/accessibilator/src/components/UI/MyModal.tsx
+++ App/Frontend/accessibilator/src/components/UI/MyModal.tsx
@@ -40,10 +40,10 @@ const MyModal: FC<PropsWithChildren<MyModalProps>> = ({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-white p-8 pb-6 pt-10 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-3xl font-medium leading-6 text-gray-900"
                   >
                     {title}
                   </Dialog.Title>
