85a27fed609ba9b1d6519631b5c4ac6b642a14ef
Dave <davetherubix@gmail.com>
2023-11-03 08:50:42 +0100

    impl. new feedback form fields UI

--- App/Frontend/accessibilator/src/components/FeedbackForm/FeedbackForm.tsx
+++ App/Frontend/accessibilator/src/components/FeedbackForm/FeedbackForm.tsx
@@ -3,6 +3,7 @@ import MyModal from '../UI/MyModal';
 import MyInput from '../UI/inputs/MyInput';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import MyTextArea from '../UI/inputs/MyTextArea';
+import Button from '../UI/Button';
 
 interface FeedbackFormProps {
   isShowing: boolean;
@@ -38,41 +39,101 @@ const FeedbackForm: FC<FeedbackFormProps> = ({
     <MyModal
       isOpen={isShowing}
       onModalClose={onFeedBackClose}
-      title="Send us your feedback"
+      title={'Send us your feedback'}
     >
-      <div>
+      <div className="mt-6">
         <MyInput
           id="Email"
-          label={'Email'}
+          label={'Email address (optional)'}
           inputRef={register('email', {
             required: false,
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: 'Enter a valid email address',
+            },
           })}
           groupClasses="mb-4"
           errors={errors}
-          className="pl-10 text-base"
-          labelProps={{ className: 'text-base' }}
+          className=""
+          labelProps={{ className: '' }}
           autoComplete="email"
           defaultValue={''}
+          placeholder="e.g jamie@gmail.com"
         ></MyInput>
         <MyTextArea
           id="whatUserLiked"
-          label={'What did you like about our application?'}
+          label={'What did you like? (*)'}
           className=""
-          groupClasses="mb-0"
-          placeholder="Tell us a little about yourself..."
+          groupClasses="mb-4"
+          placeholder="Share your experience..."
           inputRef={register('whatUserLiked', {
             minLength: {
               value: 10,
               message: 'Tell us a bit more',
             },
             maxLength: {
-              value: 500,
+              value: 400,
+              message: 'In fewer words maybe? (Max 400 chars.)',
+            },
+          })}
+          defaultValue={''}
+          errors={errors}
+          rows={4}
+        />
+        <MyTextArea
+          id="whatUserDisliked"
+          label={'What can we do better? (*)'}
+          className=""
+          groupClasses="mb-4"
+          placeholder="Share your experience..."
+          inputRef={register('whatUserDisliked', {
+            minLength: {
+              value: 10,
+              message: 'Tell us a bit more',
+            },
+            maxLength: {
+              value: 400,
+              message: 'In fewer words maybe? (Max 400 chars.)',
+            },
+          })}
+          defaultValue={''}
+          errors={errors}
+          rows={4}
+        />
+        <MyTextArea
+          id="newFeatures"
+          label={'Any new feature(s) you would like to see?'}
+          className=""
+          groupClasses="mb-0"
+          placeholder="Share your experience..."
+          inputRef={register('newFeatures', {
+            minLength: {
+              value: 10,
+              message: 'Tell us a bit more',
+            },
+            maxLength: {
+              value: 400,
               message: 'In fewer words maybe? (Max 500 chars.)',
             },
           })}
           defaultValue={''}
           errors={errors}
-          rows={8}
+          rows={4}
+        />
+      </div>
+
+      <div className="p-1 flex justify-end mt-5 mb-2">
+        <Button
+          role="navigation"
+          variant="link"
+          className="text-yellow-950 border border-yellow-950 mr-10 py-2 px-6 text-base font-medium"
+          text={'Cancel'}
+          onClick={() => {}}
+        />
+        <Button
+          className="text-base py-2 px-6 text-zinc-50 font-medium"
+          text={'Send'}
+          onClick={() => {}}
         />
       </div>
     </MyModal>
