239970276759301e7a4bafb03617d11d9951d1dd
Dave <davetherubix@gmail.com>
2023-11-09 20:02:01 +0100

    impl. getTokenApi utility function [AB#61]

--- App/Frontend/accessibilator/src/services/axios.ts
+++ App/Frontend/accessibilator/src/services/axios.ts
@@ -2,9 +2,15 @@ import Axios, { AxiosResponse } from 'axios';
 import { STORAGE_KEYS } from '../configs/constants';
 
 const axiosInit = Axios.create({
-  // baseURL: 'http://127.0.0.1:8080',
   baseURL: 'https://hn6noz98uf.execute-api.eu-north-1.amazonaws.com',
 });
+
+const getTokenApi = async () => {
+  const accessTokenRes = Axios.get(
+    'https://hn6noz98uf.execute-api.eu-north-1.amazonaws.com/getToken'
+  );
+  return accessTokenRes;
+};
 
 // Set the AUTH token for any request
 axiosInit.interceptors.request.use(async function (config) {
@@ -12,11 +18,14 @@ axiosInit.interceptors.request.use(async function (config) {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   } else {
-    const accessTokenRes = await Axios.get(
-      'https://hn6noz98uf.execute-api.eu-north-1.amazonaws.com/getToken'
-    );
-    localStorage.setItem(STORAGE_KEYS.TOKEN, `${accessTokenRes.data}`);
-    config.headers.Authorization = `Bearer ${accessTokenRes.data}`;
+    try {
+      const accessTokenRes = await getTokenApi();
+      localStorage.setItem(STORAGE_KEYS.TOKEN, `${accessTokenRes.data}`);
+      config.headers.Authorization = `Bearer ${accessTokenRes.data}`;
+    } catch (error) {
+      console.log(error);
+      //TODO: Toast error
+    }
   }
 
   return config;
@@ -31,13 +40,17 @@ axiosInit.interceptors.response.use(
     const originalRequest = error.config;
     if (error.response.status === 403 && !originalRequest._retry) {
       originalRequest._retry = true;
-      const accessTokenRes = await Axios.get(
-        'https://hn6noz98uf.execute-api.eu-north-1.amazonaws.com/getToken'
-      );
-      Axios.defaults.headers.common[
-        'Authorization'
-      ] = `Bearer ${accessTokenRes.data}`;
-      localStorage.setItem(STORAGE_KEYS.TOKEN, `${accessTokenRes.data}`);
+
+      try {
+        const accessTokenRes = await getTokenApi();
+        Axios.defaults.headers.common[
+          'Authorization'
+        ] = `Bearer ${accessTokenRes.data}`;
+        localStorage.setItem(STORAGE_KEYS.TOKEN, `${accessTokenRes.data}`);
+      } catch (error) {
+        console.log(error);
+        //TODO: Toast error
+      }
       return axiosInit(originalRequest);
     }
     return Promise.reject(error);
