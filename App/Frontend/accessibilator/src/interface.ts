b4c86246f0cccd0e601c71de486518d46600c2e7
Dave <davetherubix@gmail.com>
2023-11-13 20:42:29 +0100

    impl. modify file query params in review screen

---
+++ App/Frontend/accessibilator/src/interface.ts
@@ -0,0 +1,8 @@
+interface DocModifyParams {
+  fontSize: number;
+  lineSpacing: number;
+  fontColor: string;
+  characterSpacing: number;
+  backgroundColor: string;
+  alignment: 'LEFT' | 'RIGHT' | 'CENTRE';
+}
