f9e00abee89ed1d63c9a4320a96e94e2cc32ddf7
deny5596 <mtdeny5596@gmail.com>
2023-11-15 16:56:41 +0000

    [Feature] (Mountdenyraj Nadar) Adding tabs to the UI for customization panel

--- App/Frontend/accessibilator/src/pages/reader.tsx
+++ App/Frontend/accessibilator/src/pages/reader.tsx
@@ -6,6 +6,7 @@ import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 import SlideModal from '../components/UI/SlideModal';
 import { PencilIcon } from '@heroicons/react/24/outline';
 import { useRouter } from 'next/router';
+import { Tab } from '@headlessui/react';
 
 type Props = {};
 
@@ -239,39 +240,50 @@ const Reader = (props: Props) => {
       >
         {/* Tab navigation */}
         <div className='mb-4 flex'>
-          <>
-            <Button
-              className={`flex-1 px-4 py-2 ${
-                activeTab === 1
-                  ? 'bg-gray-500 text-white'
-                  : 'bg-gray-300 text-gray-700'
-              }`}
-              onClick={() => setActiveTab(1)}
-              text={'Text Customization'}
-            />
-          </>
-          <>
-            <Button
-              className={`flex-1 px-4 py-2 ${
-                activeTab === 1
-                  ? 'bg-gray-500 text-white'
-                  : 'bg-gray-300 text-gray-700'
-              }`}
-              onClick={() => setActiveTab(1)}
-              text={'Text Colour Customization'}
-            />
-          </>
-          <>
-            <Button
-              className={`flex-1 px-4 py-2 ${
-                activeTab === 1
-                  ? 'bg-gray-500 text-white'
-                  : 'bg-gray-300 text-gray-700'
-              }`}
-              onClick={() => setActiveTab(1)}
-              text={'Advanced Features'}
-            />
-          </>
+          <div className='border-b border-gray-200 dark:border-gray-700'>
+            <ul className='-mb-px flex flex-wrap text-center text-sm font-medium text-gray-500 dark:text-gray-400'>
+              <li className='me-2'>
+                <a
+                  href='#'
+                  className='group inline-flex items-center justify-center rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300'
+                >
+                  <svg
+                    className='me-2 h-4 w-4 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300'
+                    aria-hidden='true'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='currentColor'
+                    viewBox='0 0 20 20'
+                  >
+                    <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z' />
+                  </svg>
+                  Text customization
+                </a>
+              </li>
+              <li className='me-2'>
+                <a
+                  href='#'
+                  className='active group inline-flex items-center justify-center rounded-t-lg border-b-2 border-blue-600 p-4 text-blue-600 dark:border-blue-500 dark:text-blue-500'
+                  aria-current='page'
+                >
+                  <svg
+                    className='me-2 h-4 w-4 text-blue-600 dark:text-blue-500'
+                    aria-hidden='true'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='currentColor'
+                    viewBox='0 0 18 18'
+                  >
+                    <path d='M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z' />
+                  </svg>
+                  Text colour customization
+                </a>
+              </li>
+              <li>
+                <a className='inline-block cursor-not-allowed rounded-t-lg p-4 text-gray-400 dark:text-gray-500'>
+                  Advanced Features
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
 
         {/* Content based on active tab */}
