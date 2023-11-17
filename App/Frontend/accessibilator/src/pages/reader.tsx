6f6fb391223a0af7dedf9c449e059ffd22010ea7
Dave <davetherubix@gmail.com>
2023-11-17 06:28:14 +0000

    change refine button icon

--- App/Frontend/accessibilator/src/pages/reader.tsx
+++ App/Frontend/accessibilator/src/pages/reader.tsx
@@ -4,11 +4,12 @@ import Head from 'next/head';
 import Button from '../components/UI/Button';
 import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 import SlideModal from '../components/UI/SlideModal';
-import { PencilIcon } from '@heroicons/react/24/outline';
 import { useRouter } from 'next/router';
 import CustomisationPanel from '../components/CustomisationPanel/CustomisationPanel';
 import Link from 'next/link';
 import axiosInit from '../services/axios';
+import { FaWandMagicSparkles } from 'react-icons/fa6';
+import { HiArrowNarrowLeft } from 'react-icons/hi';
 
 type Props = {};
 
@@ -42,11 +43,13 @@ const Reader = (props: Props) => {
         console.log(docRes);
         setCurrDocData(docRes);
       })
-      .catch((err) => {});
+      .catch((err) => {
+        console.log(err);
+      });
 
     return () => {};
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [doc_id]);
 
   const docUri = currDocData?.versions.currentVersion.url;
 
@@ -90,44 +93,46 @@ const Reader = (props: Props) => {
 
   const memoisedReader = useMemo(() => {
     return (
-      <DocViewer
-        requestHeaders={{
-          'Access-Control-Allow-Origin': '*',
-        }}
-        prefetchMethod='GET'
-        documents={[
-          {
-            uri: docUri,
-          },
-        ]}
-        pluginRenderers={DocViewerRenderers}
-        style={{
-          flex: 1,
-          backgroundColor: 'red',
-        }}
-        config={{}}
-        className='my-doc-viewer-style'
-      />
+      docUri && (
+        <DocViewer
+          requestHeaders={{
+            'Access-Control-Allow-Origin': '*',
+          }}
+          prefetchMethod='GET'
+          documents={[
+            {
+              uri: docUri,
+            },
+          ]}
+          pluginRenderers={DocViewerRenderers}
+          style={{
+            flex: 1,
+            backgroundColor: 'red',
+          }}
+          config={{}}
+          className='my-doc-viewer-style'
+        />
+      )
     );
   }, [docUri]);
 
   return (
-    <DefaultLayout variant='dark'>
+    <DefaultLayout title='Document Reader' variant='dark'>
       <Head>
-        <title>Accessibilator</title>
+        <title>Accessibilator | Document Reader</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <nav className='flex items-center justify-between border-b border-stone-800 px-20 py-3'>
+      <nav className='flex items-center justify-between border-b border-stone-800 px-16 py-3'>
         <div>
-          <Button
-            role='navigation'
-            variant='link'
-            className=' px-6 py-2 text-base font-medium text-stone-700'
-            text={'Back to homepage'}
-            onClick={() => {
-              router.push('/');
-            }}
-          />
+          <Link
+            href='/'
+            className='btn-link inline-flex items-center px-2 py-2 text-base font-medium text-stone-700'
+          >
+            <span aria-hidden='true'>
+              <HiArrowNarrowLeft className='mr-3 mt-[0.125rem] h-6 w-6' />
+            </span>
+            Back to homepage
+          </Link>
         </div>
 
         <div>
@@ -160,7 +165,7 @@ const Reader = (props: Props) => {
           <Button
             className='mt-10 inline-flex flex-col gap-y-3 rounded-3xl px-6 py-9 text-base font-medium'
             text={'Refine'}
-            icon={<PencilIcon className='h-6 w-6' />}
+            icon={<FaWandMagicSparkles className='h-7 w-7' />}
             onClick={() => {
               setSlideModalOpen(true);
             }}
