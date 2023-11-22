2b559056b02549c4f8d80bb933787b687d7627b1
Cheril <cheriljohn1227@gmail.com>
2023-11-22 13:59:51 +0000

    added methods to verify the token from google and fetch ans save user info from oauth api

---
+++ App/Backend/src/main/java/com/docparser/springboot/model/TokenResponse.java
@@ -0,0 +1,16 @@
+package com.docparser.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String token;
+    private  String expiry;
+}
