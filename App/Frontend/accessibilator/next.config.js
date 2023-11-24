918b8b0fdf4f76f6f42133c703d77ef21239133a
Dave Ayang <davetherubix@gmail.com>
2023-11-24 10:43:39 +0000

    add react-spectrum toast pkgs

--- App/Frontend/accessibilator/next.config.js
+++ App/Frontend/accessibilator/next.config.js
@@ -1,7 +1,64 @@
-const { withSentryConfig } = require("@sentry/nextjs");
+const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: [
+    '@adobe/react-spectrum',
+    '@react-spectrum/actionbar',
+    '@react-spectrum/actiongroup',
+    '@react-spectrum/avatar',
+    '@react-spectrum/badge',
+    '@react-spectrum/breadcrumbs',
+    '@react-spectrum/button',
+    '@react-spectrum/buttongroup',
+    '@react-spectrum/calendar',
+    '@react-spectrum/checkbox',
+    '@react-spectrum/combobox',
+    '@react-spectrum/contextualhelp',
+    '@react-spectrum/datepicker',
+    '@react-spectrum/dialog',
+    '@react-spectrum/divider',
+    '@react-spectrum/dnd',
+    '@react-spectrum/form',
+    '@react-spectrum/icon',
+    '@react-spectrum/illustratedmessage',
+    '@react-spectrum/inlinealert',
+    '@react-spectrum/image',
+    '@react-spectrum/label',
+    '@react-spectrum/labeledvalue',
+    '@react-spectrum/layout',
+    '@react-spectrum/link',
+    '@react-spectrum/list',
+    '@react-spectrum/listbox',
+    '@react-spectrum/menu',
+    '@react-spectrum/meter',
+    '@react-spectrum/numberfield',
+    '@react-spectrum/overlays',
+    '@react-spectrum/picker',
+    '@react-spectrum/progress',
+    '@react-spectrum/provider',
+    '@react-spectrum/radio',
+    '@react-spectrum/slider',
+    '@react-spectrum/searchfield',
+    '@react-spectrum/statuslight',
+    '@react-spectrum/switch',
+    '@react-spectrum/table',
+    '@react-spectrum/tabs',
+    '@react-spectrum/tag',
+    '@react-spectrum/text',
+    '@react-spectrum/toast',
+    '@react-spectrum/textfield',
+    '@react-spectrum/theme-dark',
+    '@react-spectrum/theme-default',
+    '@react-spectrum/theme-light',
+    '@react-spectrum/tooltip',
+    '@react-spectrum/view',
+    '@react-spectrum/well',
+    '@spectrum-icons/illustrations',
+    '@spectrum-icons/ui',
+    '@spectrum-icons/workflow',
+  ],
+};
 
 module.exports = withSentryConfig(
   nextConfig,
@@ -11,8 +68,8 @@ module.exports = withSentryConfig(
 
     // Suppresses source map uploading logs during build
     silent: true,
-    org: "bytethebarrier",
-    project: "javascript-nextjs",
+    org: 'bytethebarrier',
+    project: 'javascript-nextjs',
   },
   {
     // For all available options, see:
@@ -25,7 +82,7 @@ module.exports = withSentryConfig(
     transpileClientSDK: true,
 
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
+    tunnelRoute: '/monitoring',
 
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
