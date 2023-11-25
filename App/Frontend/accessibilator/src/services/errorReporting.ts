5db3f29bdd7fd7d9f2e9209a67f026572b3b44d9
Dave <davetherubix@gmail.com>
2023-11-25 12:49:04 +0100

    add error detail to sentry breadcrumb

--- App/Frontend/accessibilator/src/services/errorReporting.ts
+++ App/Frontend/accessibilator/src/services/errorReporting.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/nextjs';
+import { isAxiosError } from 'axios';
 
 export const reportException = (
   err: unknown,
@@ -7,6 +8,9 @@ export const reportException = (
   Sentry.addBreadcrumb({
     level: 'error',
     ...breadcrumb,
+    message: isAxiosError(err)
+      ? `${breadcrumb?.message}: ${err?.response?.data?.detail || err?.message}`
+      : breadcrumb.message,
   });
 
   Sentry.captureException(err);
