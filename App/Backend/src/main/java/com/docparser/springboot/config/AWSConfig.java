65c4885057d94ea6bb0666d3dadaf13deced0a4b
Cheril <cheriljohn1227@gmail.com>
2023-11-26 19:24:53 +0000

    moved config files to config package

--- App/Backend/src/main/java/com/docparser/springboot/config/AWSConfig.java
+++ App/Backend/src/main/java/com/docparser/springboot/config/AWSConfig.java
@@ -1,4 +1,4 @@
-package com.docparser.springboot;
+package com.docparser.springboot.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
