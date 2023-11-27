1ef4138f1d64e4dda21d72372be276831a5ac320
Mountdenyraj Nadar <D22124430@mytudublin.ie>
2023-11-27 11:14:38 +0100

    [Feature] (Mountdenyraj Nadar) Adding comments to DocumentModifier.java

--- App/Backend/src/main/java/com/docparser/springboot/service/DocumentModifier.java
+++ App/Backend/src/main/java/com/docparser/springboot/service/DocumentModifier.java
@@ -1,9 +1,10 @@
 package com.docparser.springboot.service;
 
+// Importing essential packages for the DocumentModifier Interface
 import com.docparser.springboot.model.DocumentConfig;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
-
+// Interface for DocumentModifier which uses Apache POI to modify the document's metadata
 public interface DocumentModifier {
 
     XWPFDocument modify(XWPFDocument document, DocumentConfig config);
