f1b707a695281858dfd0d111ac4eecaba9a066a9
Dave <davetherubix@gmail.com>
2023-11-27 17:06:42 +0100

    change toggle states colors

--- App/Frontend/accessibilator/src/components/UI/MyToggle.tsx
+++ App/Frontend/accessibilator/src/components/UI/MyToggle.tsx
@@ -8,11 +8,11 @@ interface MyToggleProps extends SwitchProps<any> {
 const MyToggle: FC<MyToggleProps> = ({ ariaLabel, ...toggleProps }) => {
   return (
     <Switch
-      className='relative inline-flex h-6 w-11 items-center rounded-full ui-checked:bg-yellow-600 ui-checked:bg-opacity-70 ui-not-checked:bg-stone-400'
+      className='relative inline-flex h-6 w-11 items-center rounded-full ui-checked:bg-green-700 ui-checked:bg-opacity-70 ui-not-checked:bg-stone-400'
       {...toggleProps}
     >
       <span className='sr-only'>{ariaLabel}</span>
-      <span className='inline-block h-4 w-4 transform rounded-full transition ui-checked:!translate-x-6 ui-checked:!bg-yellow-950 ui-not-checked:translate-x-1 ui-not-checked:bg-stone-200' />
+      <span className='inline-block h-4 w-4 transform rounded-full transition ui-checked:!translate-x-6 ui-checked:!bg-slate-50 ui-not-checked:translate-x-1 ui-not-checked:bg-stone-500' />
     </Switch>
   );
 };
