430fd9eb2a388d3de04d36af73b8564c7864ce1d
Cheril <cheriljohn1227@gmail.com>
2023-11-28 19:13:37 +0000

    changed modifyFile api to post call

--- App/Backend/src/main/java/com/docparser/springboot/controller/DocxController.java
+++ App/Backend/src/main/java/com/docparser/springboot/controller/DocxController.java
@@ -4,6 +4,7 @@ import com.docparser.springboot.model.DocumentConfig;
 import com.docparser.springboot.model.DocumentResponse;
 import com.docparser.springboot.model.S3StorageInfo;
 import com.docparser.springboot.service.DocumentParser;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
 
 @CrossOrigin
@@ -26,28 +28,20 @@ public class DocxController {
 
 
     @PostMapping("/uploadFile")
-    public ResponseEntity<S3StorageInfo> fileUploading(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<S3StorageInfo> fileUploading(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
         // Code to save the file to a database or disk
+        Optional<String> token = Optional.of(request.getHeader("Authorization"));
         S3StorageInfo storageInfo = documentParser.uploadFile(file);
         return ResponseEntity.ok(storageInfo);
     }
 
 
-    @GetMapping("/modifyFile")
+    @PostMapping ("/modifyFile")
     public ResponseEntity<DocumentResponse> modifyDocument(@RequestParam("filename") String fileName, @RequestParam("docID") String docID, @RequestParam("versionID") String versionID,
-                                                           @RequestParam(required = false) String fontType,
-                                                           @RequestParam(required = false) String fontSize,
-                                                           @RequestParam(required = false) String fontColor,
-                                                           @RequestParam(required = false) String lineSpacing,
-                                                           @RequestParam(required = false) String characterSpacing,
-                                                           @RequestParam(required = false) String backgroundColor,
-                                                           @RequestParam(required = false) String alignment,
-                                                           @RequestParam(required = false) Boolean generateTOC,
-                                                           @RequestParam(required = false) Boolean removeItalics) throws IOException {
+                                                           @RequestBody DocumentConfig documentConfig) throws IOException {
+
         // Code to save the file to a database or disk
-        DocumentResponse storageInfo = documentParser.modifyFile(fileName, docID, versionID,
-                new DocumentConfig(fontType, fontSize, fontColor, backgroundColor, lineSpacing, characterSpacing,
-                        alignment, generateTOC, removeItalics));
+        DocumentResponse storageInfo = documentParser.modifyFile(fileName, docID, versionID, documentConfig);
         return ResponseEntity.ok(storageInfo);
     }
 
