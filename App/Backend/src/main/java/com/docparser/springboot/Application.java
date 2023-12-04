8a4e49f42c927b969867c71a8e48a3ddf82dbff0
Mountdenyraj Nadar <D22124430@mytudublin.ie>
2023-12-04 13:42:21 +0100

    [Feature] (Mountdenyraj Nadar) Adding comments to Application.java

--- App/Backend/src/main/java/com/docparser/springboot/Application.java
+++ App/Backend/src/main/java/com/docparser/springboot/Application.java
@@ -1,15 +1,18 @@
 package com.docparser.springboot;
 
+// Importing necessary classes from the Spring Boot framework
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// Annotation to mark this class as a Spring Boot Application
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		// Running the Spring Boot application
+		// This method will bootstrap the application, starting the Spring Application
+		// Context
 		SpringApplication.run(Application.class, args);
 	}
-
 
 }
