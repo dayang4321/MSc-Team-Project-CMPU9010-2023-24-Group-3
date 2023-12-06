97d47265b368149eef25d1609d85aeaf8f5aeb06
Mountdenyraj Nadar <D22124430@mytudublin.ie>
2023-12-06 15:38:21 +0100

    [Feature] (Mountdenyraj Nadar) Adding comments to DocumentInfo.java

--- App/Backend/src/main/java/com/docparser/springboot/model/DocumentInfo.java
+++ App/Backend/src/main/java/com/docparser/springboot/model/DocumentInfo.java
@@ -7,11 +7,15 @@ import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbParti
 import java.time.Instant;
 import java.util.List;
 
+// Annotation indicating that this class is a DynamoDB bean.
 @DynamoDbBean
 public class DocumentInfo {
 
+    /*
+     * Fields to store the document's ID, key, creation and expiration times,
+     * configuration, and version information
+     */
     private String documentID;
-
     private String documentKey;
 
     private Instant createdDate;
@@ -20,52 +24,73 @@ public class DocumentInfo {
     private DocumentConfig documentConfig;
 
     private List<VersionInfo> documentVersions;
+
+    /*
+     * Getter for document ID. Annotated as both a DynamoDB attribute and a
+     * partition key.
+     */
     @DynamoDbAttribute("documentID")
     @DynamoDbPartitionKey
     public String getDocumentID() {
         return documentID;
     }
 
+    // Setter for document ID
     public void setDocumentID(String documentID) {
         this.documentID = documentID;
     }
+
+    // Getter for document configuration
     @DynamoDbAttribute("documentConfig")
     public DocumentConfig getDocumentConfig() {
         return documentConfig;
     }
 
+    // Setter for document configuration
     public void setDocumentConfig(DocumentConfig documentConfig) {
         this.documentConfig = documentConfig;
     }
+
+    // Getter for the list of document versions
     @DynamoDbAttribute("documentVersions")
     public List<VersionInfo> getDocumentVersions() {
         return documentVersions;
     }
 
+    // Setter for the list of document versions
     public void setDocumentVersions(List<VersionInfo> documentVersions) {
         this.documentVersions = documentVersions;
     }
 
+    // Getter for document key
     @DynamoDbAttribute("documentKey")
     public String getDocumentKey() {
         return documentKey;
     }
-    public  void setDocumentKey(String documentKey) {
+
+    // Setter for document key
+    public void setDocumentKey(String documentKey) {
         this.documentKey = documentKey;
     }
+
+    // Getter for created date
     @DynamoDbAttribute("createdDate")
     public Instant getCreatedDate() {
         return createdDate;
     }
 
+    // Setter for created date
     public void setCreatedDate(Instant createdDate) {
         this.createdDate = createdDate;
     }
+
+    // Getter for expiration time
     @DynamoDbAttribute("expirationTime")
     public Instant getExpirationTime() {
         return expirationTime;
     }
 
+    // Setter for expiration time
     public void setExpirationTime(Instant expirationTime) {
         this.expirationTime = expirationTime;
     }
