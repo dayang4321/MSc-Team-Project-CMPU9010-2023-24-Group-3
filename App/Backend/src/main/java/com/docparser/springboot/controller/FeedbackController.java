3262cf1dd22598d22e15c1108a1e1218bef34f23
Mountdenyraj Nadar <D22124430@mytudublin.ie>
2023-12-07 19:08:54 +0100

    [Feature] (Mountdenyraj Nadar) Adding comments to FeedbackController.java

--- App/Backend/src/main/java/com/docparser/springboot/controller/FeedbackController.java
+++ App/Backend/src/main/java/com/docparser/springboot/controller/FeedbackController.java
@@ -16,36 +16,40 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-
 @CrossOrigin
 @RestController
-@RequestMapping("/api/feedback")
+@RequestMapping("/api/feedback") // Sets the base URL for all methods in this controller
 public class FeedbackController {
+    // Logger instance for logging information
     Logger logger = LoggerFactory.getLogger(FeedbackController.class);
+
+    // Auto-injecting dependencies
     @Autowired
     private ObjectMapper objectMapper;
     @Autowired
     private SessionService sessionService;
 
-
     @ResponseBody
     @PostMapping("/save")
-    public ResponseEntity<String> postFeedback(@RequestBody FeedBackForm feedBackForm, HttpServletRequest request) throws JsonProcessingException {
+    public ResponseEntity<String> postFeedback(@RequestBody FeedBackForm feedBackForm, HttpServletRequest request)
+            throws JsonProcessingException {
         // Code to save the file to a database or disk
         Optional<String> token = Optional.of(request.getHeader("Authorization"));
+
         sessionService.saveFeedbackInfo(token.get().substring(7), feedBackForm);
         return ResponseEntity.ok("Feedback saved successfully");
     }
 
-
-    @GetMapping("/fetch")
+    @GetMapping("/fetch") // Maps HTTP GET requests to /api/feedback/fetch
     public ResponseEntity<Object> getFeedback(HttpServletRequest request) throws JsonProcessingException {
         // Code to save the file to a database or disk
         Optional<String> token = Optional.of(request.getHeader("Authorization"));
         List<FeedBackForm> feedBackForms = sessionService.getFeedBackForm(token.get().substring(7));
         logger.info("feedbackInfo fetched from DB " + objectMapper.writeValueAsString(feedBackForms));
+
+        // Prepares the response with the fetched data
         Map<String, Object> object = new HashMap<>();
-        object.put("feedbackForms",feedBackForms);
+        object.put("feedbackForms", feedBackForms);
         return ResponseEntity.ok(object);
     }
 
