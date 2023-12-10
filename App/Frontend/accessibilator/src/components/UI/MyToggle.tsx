ce9c482b14fa2fc6af0d5f3bf7830966c19724dd
Mountdenyraj Nadar <D22124430@mytudublin.ie>
2023-12-10 15:38:55 +0100

    [Feature] (Mountdenyraj Nadar) Updating and cleaning the comments in MyToggle.tsx

--- App/Frontend/accessibilator/src/components/UI/MyToggle.tsx
+++ App/Frontend/accessibilator/src/components/UI/MyToggle.tsx
@@ -1,18 +1,22 @@
 import { FC } from 'react';
 import { Switch, SwitchProps } from '@headlessui/react';
 
-// Interface extending the standard SwitchProps from Headless UI
-// with an additional ariaLabel property for accessibility.
+/**
+ * Interface extending the standard SwitchProps from Headless UI
+ * with an additional ariaLabel property for accessibility
+ */
 interface MyToggleProps extends SwitchProps<any> {
   ariaLabel: string;
 }
 
-// Functional component MyToggle, utilizing the extended properties.
+// Defining the functional component as MyToggle, utilizing the extended properties
 const MyToggle: FC<MyToggleProps> = ({ ariaLabel, ...toggleProps }) => {
   return (
-    // The Switch component from Headless UI is used to create a toggle switch.
-    // className defines the styling of the toggle switch, including dimensions,
-    // background color, and rounded corners.
+    /**
+     * The Switch component from Headless UI is used to create a toggle switch.
+     * className defines the styles related to the toggle switch, including dimensions,
+     * background color, and rounded corners.
+     */
     <Switch
       className='relative inline-flex h-6 w-11 items-center rounded-full ui-checked:bg-green-700 ui-checked:bg-opacity-70 ui-not-checked:bg-stone-400'
       {...toggleProps}
@@ -21,12 +25,11 @@ const MyToggle: FC<MyToggleProps> = ({ ariaLabel, ...toggleProps }) => {
           The ariaLabel prop provides the necessary descriptive label. */}
       <span className='sr-only'>{ariaLabel}</span>
 
-      {/* This span represents the toggle button itself. It changes position and color based on the toggle state.
-          The 'transform' and 'transition' classes enable smooth movement and transition effects. */}
+      {/* The span element represents the toggle button and it changes the position and colour based on the state of the toggle.
+          The 'transform' and 'transition' classes enable the smooth movement and transition effects. */}
       <span className='inline-block h-4 w-4 transform rounded-full transition ui-checked:!translate-x-6 ui-checked:!bg-slate-50 ui-not-checked:translate-x-1 ui-not-checked:bg-stone-500' />
     </Switch>
   );
 };
 
-// Export the MyToggle component for use in other parts of the application.
 export default MyToggle;
