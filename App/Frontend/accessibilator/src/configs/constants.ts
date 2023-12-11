ff422e7ac2e9cab0c6aa55dde09b44d9125b2730
Mountdenyraj Nadar <D22124430@mytudublin.ie>
2023-12-11 15:23:17 +0100

    [Feature] (Mountdenyraj Nadar) Adding comments to configs.ts and constants.ts

--- App/Frontend/accessibilator/src/configs/constants.ts
+++ App/Frontend/accessibilator/src/configs/constants.ts
@@ -1,5 +1,19 @@
 export const STORAGE_KEYS = {
+  /**
+   * This constant is used to store and retrieve the access token from a storage mechanism.
+   * The access token is typically used for authentication purposes in an application.
+   */
   TOKEN: 'ACCESSIBILATOR@TOKEN',
+
+  /**
+   * This constant is used to store and retrieve the expiry time of the token.
+   * Keeping track of the expiry time is important to ensure that the application requests a new token when the current one expires.
+   */
   EXPIRY: 'ACCESSIBILATOR@EXPIRY',
+
+  /**
+   * This constant is used to store and retrieve user information.
+   * This includes the details such as the user's ID, name, or other personal information.
+   */
   USER: 'ACCESSIBILATOR@USER',
 };
