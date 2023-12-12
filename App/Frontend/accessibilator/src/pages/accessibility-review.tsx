a0f7d6de4cb27338a07d54e3c39f26711b0eadfc
Mountdenyraj Nadar <D22124430@mytudublin.ie>
2023-12-12 14:45:09 +0100

    [Feature] (Mountdenyraj Nadar) Adding the comments to accessibility-review.tsx

--- App/Frontend/accessibilator/src/pages/accessibility-review.tsx
+++ App/Frontend/accessibilator/src/pages/accessibility-review.tsx
@@ -10,6 +10,7 @@ import { reportException } from '../services/errorReporting';
 import { ToastQueue } from '@react-spectrum/toast';
 import { AuthContext } from '../contexts/AuthContext';
 
+// Define the type for the primary fixes that can be applied to a document
 type PrimaryFix =
   | 'fontStyle'
   | 'fontSize'
@@ -19,18 +20,24 @@ type PrimaryFix =
   | 'italics'
   | 'alignment';
 
+// Define the main functional component for reviewing accessibility settings
 export default function AccessibilityReview() {
+  // React hooks for updating the loading states of document modification
   const [isModifyLoading, setIsModifyLoading] = useState(false);
   const router = useRouter();
   const { doc_id, doc_key, version_id } = router.query;
 
+  // Use context to access the authenticated user's information
   const { user } = useContext(AuthContext);
 
+  // Retrieve the saved user presets if it's available
   const currentUserPresets = user?.userPresets;
 
+  //Managing the choices regarding document modification
   const [choicesObj, setChoicesObj] = useState<Record<PrimaryFix, boolean>>(
     user
       ? {
+          // Initialize state with user presets if available
           fontStyle: !!user?.userPresets?.fontType,
           fontSize: !!user?.userPresets?.fontSize,
           interSpacing: !!user?.userPresets?.characterSpacing,
@@ -43,6 +50,7 @@ export default function AccessibilityReview() {
           alignment: !!user?.userPresets?.alignment,
         }
       : {
+          // Default settings if no user presets are available
           fontStyle: true,
           fontSize: true,
           interSpacing: true,
@@ -53,6 +61,7 @@ export default function AccessibilityReview() {
         }
   );
 
+  // Set the toggle choice handler function
   const setChoiceHandler = (choice: PrimaryFix) => {
     setChoicesObj((s) => ({
       ...s,
@@ -60,9 +69,12 @@ export default function AccessibilityReview() {
     }));
   };
 
+  // Defining the functional component: OnReviewConfirm, to handle for confirming review changes
   const onReviewConfirm = () => {
+    // Set the loading state to true
     setIsModifyLoading(true);
 
+    // Define the DocModifyParams which indicate the different document modification parameters
     const docModParams: Partial<DocModifyParams> = {
       fontType: choicesObj.fontStyle
         ? currentUserPresets?.fontType || 'arial'
@@ -90,6 +102,7 @@ export default function AccessibilityReview() {
         : undefined,
     };
 
+    // Make a POST request to the modifyFile API endpoint
     axiosInit
       .post<DocumentData>(
         '/api/file/modifyFile',
@@ -103,7 +116,8 @@ export default function AccessibilityReview() {
         }
       )
       .then((res) => {
-        //  console.log(res);
+        // On successful response, navigate to the reader page with the updated document
+        // console.log(res);
         router.push({
           pathname: '/reader',
           query: {
@@ -112,7 +126,8 @@ export default function AccessibilityReview() {
         });
       })
       .catch((err) => {
-        //  console.log(err);
+        // On error, display a toast message and log the exception
+        // console.log(err);
         ToastQueue.negative(
           `An error occurred! ${
             err?.response?.data.detail || err?.message || ''
@@ -130,10 +145,12 @@ export default function AccessibilityReview() {
         });
       })
       .finally(() => {
+        // Reset the loading state after receiving the data or handling an error if the API fails
         setIsModifyLoading(false);
       });
   };
 
+  // Render the component UI for the accessibility review page
   return (
     <DefaultLayout>
       <Head>
@@ -141,7 +158,9 @@ export default function AccessibilityReview() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
+      {/* Main content area */}
       <main className='flex flex-1 flex-col items-center justify-center bg-slate-50 py-14 pb-8 text-center text-base text-gray-900'>
+        {/* Accessibility review section */}
         <div className='text-center'>
           <h1 className='mb-7 text-4xl font-bold'>
             The document has been uploaded successfully
@@ -150,7 +169,9 @@ export default function AccessibilityReview() {
             Here are some changes suggested to make your document easier to read
           </h3>
 
+          {/* Options for modifying the document's accessibility settings */}
           <div className='mx-auto mt-12 max-w-[50rem] divide-y divide-solid rounded-md border border-gray-600 p-7'>
+            {/* Toggle for font size increase */}
             <div className='flex items-center justify-between py-5'>
               <InfoTooltip infoTip='Larger font sizes (12-14 pt.) makes reading easier, especially for readers who may find smaller text challenging to follow'>
                 <p className='text-lg'>Increase font size</p>
@@ -226,7 +247,9 @@ export default function AccessibilityReview() {
           </div>
         </div>
 
+        {/* Button section for navigation */}
         <div className='mt-8 w-full max-w-4xl text-right '>
+          {/* Back button */}
           <Button
             variant='link'
             className=' mr-10 border border-yellow-900 px-6 py-2 text-base font-medium'
@@ -235,6 +258,7 @@ export default function AccessibilityReview() {
               router.back();
             }}
           />
+          {/* Continue button */}
           <Button
             className=' px-6 py-2 text-base'
             loading={isModifyLoading}
